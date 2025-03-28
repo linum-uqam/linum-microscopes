e592df448f4b4563d02e92a83d4fd214476681fa
Joël Lefebvre <lefebvre.joel@uqam.ca>
2025-03-28 10:49:55 -0400

    Added parameters to functions

--- linum_microscopes/controllers/abstract_camera.py
+++ linum_microscopes/controllers/abstract_camera.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 
+import numpy as np
+
 from .abstract_device import AbstractDevice
 
 class AbstractCamera(AbstractDevice):
@@ -8,21 +10,21 @@ class AbstractCamera(AbstractDevice):
         super().__init__(config)
 
     @abstractmethod
-    def save_data(self):
+    def save_data(self ,path: str):
         raise NotImplementedError("Method not implemented")
 
     @abstractmethod
-    def return_data(self):
+    def return_data(self) -> np.ndarray:
         raise NotImplementedError("Method not implemented")
 
     @abstractmethod
-    def get_camera_settings(self):
+    def get_camera_settings(self) -> dict:
         raise NotImplementedError("Method not implemented")
 
     @abstractmethod
-    def set_camera_settings(self):
+    def set_camera_settings(self, settings: dict):
         raise NotImplementedError("Method not implemented")
 
     @abstractmethod
-    def set_acquisition_mode(self):
+    def set_acquisition_mode(self, mode: str):
         raise NotImplementedError("Method not implemented")
